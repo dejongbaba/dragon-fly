25b898b7aaea72e16cae7672a38ab7015c2a67bb
Agunbiade Adedeji <dejongbaba@gmail.com>
2024-05-16 10:46:47 +0100

    feat: add upload file support, upload file process and status monitoring

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,135 @@
-import {Button} from "@/components/ui/button";
+'use client'
+import {useEffect, useState} from "react";
+import {accessProcess, checkFileStatus, fileToBinary, generateUrl, stageFile} from "@/services/files/files";
 
 export default function Home() {
 
+    const [fileList, setFileList] = useState([]);
+    const [statuses, setStatuses] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [statusLoading, setStatusLoading] = useState(false);
+    const [processLoading, setProcessLoading] = useState(false);
+    const [keys, setKeys] = useState([]);
+    const [taskIds, setTaskIds] = useState([]);
+
+
+    //    handle multiple files on change
+    const handleChange = async (event) => {
+
+        // const formData = new FormData();
+
+        const files = event.target.files;
+        console.log('files outside', event, event.target, files, typeof files, files?.length)
+
+        if (files.length) {
+
+            try {
+                let filesArray = [];
+                setLoading(true)
+                for (let f of Object.entries(files)) {
+                    const binary = await fileToBinary(f)
+                    const formData = new FormData();
+                    formData.append("data", binary);
+                    const {key, url} = await generateUrl();
+                    // store keys to monitor file upload progress
+                    setKeys([...keys, key])
+                    filesArray.push(stageFile(url, key, binary))
+                }
+
+                console.log('files after processing ', files)
+                const uploadedFiles = await Promise.allSettled(filesArray);
+
+                setFileList(uploadedFiles);
+                setLoading(false)
+
+            } catch (e) {
+                console.log('e', e)
+                return Promise.reject(e)
+            }
+        }
+
+
+    }
+
+
+    useEffect(() => {
+        const getImageProcesses = async () => {
+            try {
+                setProcessLoading(true);
+                const processArray = keys.map((k) => {
+                    return accessProcess(k)
+                })
+                const processes = await Promise.allSettled(processArray)
+                setTaskIds(processes)
+                setProcessLoading(false);
+
+            } catch (e) {
+                return Promise.reject(e)
+            }
+
+        }
+
+        if (keys?.length) {
+            getImageProcesses()
+        }
+
+    }, [keys]);
+
+
+    useEffect(() => {
+
+        const getImageStatuses = async () => {
+            try {
+                setStatusLoading(true)
+                const statusArray = taskIds.map((k) => {
+                    return checkFileStatus(k)
+                })
+                const statuses = await Promise.allSettled(statusArray)
+                setStatuses(statuses)
+                setStatusLoading(true)
+            } catch (e) {
+                return Promise.reject(e)
+            }
+        }
+
+        if (taskIds?.length) {
+            getImageStatuses()
+        }
+
+    }, [taskIds]);
+
+
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1 className='font-semibold text-2xl'> Dragonfly Uploader</h1>
-            <p className='text-slate-400  text-base'> Click the button below to upload a file</p>
-            <div className='w-1/2 m-auto bg-slate-100 rounded-lg p-6'>
-                <Button>Upload</Button>
+        <main className="min-h-screen max-w-lg space-y-4 p-24">
+            <div className='space-y-1'>
+                <h1 className='font-semibold mb-0 text-xl leading-normal'> Dragonfly Uploader</h1>
+                <p className='text-slate-400  text-sm'> Click the button below to upload a file</p>
+            </div>
+            <div className='bg-slate-100 space-y-6 rounded-lg p-6'>
+                <form className='bg-slate-200 space-y-2 p-5 rounded-lg'>
+                    <input multiple={true} type="file" onChange={handleChange}/>
+                    {loading && <p>Uploading file...</p>}
+                </form>
+
+                <div className='space-y-2'>
+                    <h1>File Processes</h1>
+                    {processLoading && <p>loading process status...</p>}
+                    <div className='bg-slate-200 min-h-[100px] rounded-lg'>
+                        {/*    processes would be shown here */}
+                    </div>
+
+                </div>
+                <div className='space-y-2'>
+
+                    <h1>File Statuses</h1>
+                    {statusLoading && <p>loading file status...</p>}
+                    <div className='bg-slate-200 min-h-[100px] rounded-lg'>
+                        {/*    statuses would be shown here */}
+
+                    </div>
+
+                </div>
+
             </div>
         </main>
     );
---
+++ src/services/files/files.js
@@ -0,0 +1,71 @@
+import {api} from "@/services";
+
+const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+
+const fileToBinary = (file) => {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = () => {
+            const binaryData = reader.result;
+            resolve(binaryData);
+        };
+        reader.onerror = () => {
+            reject(new Error('Unable to read the file as binary data'));
+        };
+        console.log('file in reader', file)
+        const blob = new Blob([file], {type: file.type})
+        reader.readAsBinaryString(blob);
+    });
+};
+
+const generateUrl = () => {
+    return api.post('/assets/stage', false,
+        {headers: {Authorization: apiKey}}).then(res => res.data)
+        .catch((e) => Promise.reject(e))
+}
+/**
+ *  upload file to aws
+ *  @param uploadUrl,key,file
+ *
+ * */
+const stageFile = async (url, key, data) => {
+
+    try {
+        const res = await api.put(url, data,
+            {
+                headers: {'Content-Type': 'multipart/form-data', 'api-key': key},
+                baseURL: ''
+            })
+        return res.data
+    } catch (e) {
+        return Promise.reject(e)
+    }
+
+}
+
+const accessProcess = (key) => {
+    return api.post(`/assets/process?key=${key}&pipeline=dragonfly-img-basic`, null, {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                Authorization: apiKey
+            }
+        },
+    ).then((res) => res.data)
+        .catch((e) => Promise.reject(e))
+}
+
+const checkFileStatus = (taskId) => {
+    return api.get('/assets/status',
+        {
+            data: {taskId},
+            headers: {
+                Authorization: apiKey,
+                'Cache-Control': 'no-store',
+                'Content-Type': 'application/json'
+            }
+        }).then(res => res.data)
+        .catch((e) => Promise.reject(e))
+}
+
+
+export {generateUrl, stageFile, accessProcess, checkFileStatus, fileToBinary}
